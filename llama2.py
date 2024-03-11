8cd7126c0c7c2ac4a65edfb6f75ec88b4692925d
ketan juikar <ketan27j@gmail.com>
2024-03-11 11:48:21 +0530

    aws bedrock demo

---
+++ llama2.py
@@ -0,0 +1,27 @@
+import boto3
+import json
+
+prompt_data = """
+Act as a Shekespeare and write a poem on machine learning.
+"""
+
+bedrock = boto3.client(service_name="bedrock-runtime")
+payload = {
+    "prompt":"[INST]"+ prompt_data + "[/INST]",
+    "max_gen_len" : 512,
+    "temperature" : 0.5,
+    "top_p" : 0.9,
+}
+
+body= json.dumps(payload)
+model_id = "meta.llama2-70b-chat-v1"
+response= bedrock.invoke_model(
+    modelId=model_id, 
+    body=body,
+    accept="application/json",
+    contentType="application/json"
+    )
+
+response_body = json.loads(response['body'].read())
+response_text = response_body['generation']
+print(response_text)
